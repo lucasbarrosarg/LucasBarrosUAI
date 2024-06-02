7f2bb09710985dd0508c6b52f4fd41d1174d1274
Lucas B Nimbux <lucas.barros@nimbux911.com>
2024-06-01 21:22:54 -0300

    Actividad asincronica clase 8

---
+++ ActividadAsincronicaClase8/exercises/variables_and_operators.js
@@ -0,0 +1,6 @@
+/* Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable. */
+var numero1=5;
+var numero2=7;
+var resultado=numero1+numero2;
+console.log(resultado);
+
